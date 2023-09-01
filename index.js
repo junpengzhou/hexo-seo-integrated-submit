51d2d0efa3e3caed7a40324ee551df3ea0b266d4
Junpengzhou <jeepzhou11@163.com>
2023-09-01 11:13:20 +0800

    refactor: rename the process of generate the crawler json file

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ const defaultConfig = {
 
 hexo.config.hexo_seo_integrated_submit = Object.assign(defaultConfig, hexo.config.hexo_seo_integrated_submit)
 
-hexo.extend.generator.register('BingSeoGenerator', require('./lib/crawler_json_generator'))
+hexo.extend.generator.register('CrawlerJsonGenerator', require('./lib/crawler_json_generator'))
 hexo.extend.generator.register('GithubActionsHexoSeo', require('./lib/actions'))
