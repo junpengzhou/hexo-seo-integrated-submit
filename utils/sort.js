1ca6535409ce8cb7d6a2431b0dcd23b8a15b3b78
Junpengzhou <jeepzhou11@163.com>
2023-09-01 11:09:48 +0800

    feat: add pnpm lock file

--- utils/sort.js
+++ utils/sort.js
@@ -1,12 +1,12 @@
-//日期排序
+// sort by date
 module.exports = { sortDownDate, sortUpDate }
 
-// 降序
+// desc
 function sortUpDate(arr) {
   return arr.sort((a, b) => Date.parse(a) - Date.parse(b))
 }
 
-// 升序
+// asc
 function sortDownDate(arr, value) {
   return arr.sort((a, b) => Date.parse(b[value]) - Date.parse(a[value]))
 }
