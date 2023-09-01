3d8b38a037c3694863cc1455301d2a42b96fe68a
Junpengzhou <jeepzhou11@163.com>
2023-09-01 11:07:19 +0800

    refactor: simply the process of hexo generator register

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const defaultConfig = {
+  
   baidu: { enable: true },
   bing: { enable: true },
   google: { enable: true }
 }
 
-hexo.config.hexo_seo_autopush = Object.assign(defaultConfig, hexo.config.hexo_seo_autopush)
+hexo.config.hexo_seo_integrated_submit = Object.assign(defaultConfig, hexo.config.hexo_seo_integrated_submit)
 
-hexo.extend.generator.register('BaiduSeoGenerator', require('./lib/baidu_generator'))
-hexo.extend.generator.register('BingSeoGenerator', require('./lib/bing_generator'))
-hexo.extend.generator.register('GoogleSeoGenerator', require('./lib/google_generator'))
+hexo.extend.generator.register('BingSeoGenerator', require('./lib/crawler_json_generator'))
 hexo.extend.generator.register('GithubActionsHexoSeo', require('./lib/actions'))
